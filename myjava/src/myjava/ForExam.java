00d3f6cfc38c025fceac2866b9e072f3096747ae
jiwoo park <h591262@naver.com>
2017-04-07 17:48:51 +0900

    2017.04.07 마지막

---
+++ myjava/src/myjava/ForExam.java
@@ -0,0 +1,35 @@
+package myjava;
+
+public class ForExam {
+
+	/*
+	 * 1.
+	 * 2x + 4y =10 이 되는 방정식의 해를 구하시오
+	 *  단, x,y는 0보다 같거나 크고 10보다 같거나 작은 정수임
+	 * 
+	 *  
+	 *  
+	 *  2.
+	 *  주사위 두개를 던졌을 때, 두눈의 합이  6이되는 경우를 출력하여라.
+	 */
+	
+	public static void main(String[] args) {
+		/*for(int x=0;x<11;x++) {
+			for(int y =0;y<11;y++) {
+				if(2*x+4*y==10) {
+					System.out.println(x+"와"+y);
+					}
+			}
+		}
+		*/
+		
+/*		for(int x=1; x<=6; x++) {
+			for(int y=1; y<=6; y++) {
+				if(x+y==6) {
+					System.out.println(x+","+y);
+				}
+			}
+		}*/
+		
+	}
+}
